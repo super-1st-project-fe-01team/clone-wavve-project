5eddc34d2d988be879b2643cbdff77b3fdc26fe5
DokyungYou <willow74rr@gmail.com>
2024-06-10 11:48:04 +0900

    Feat (Player): PlayerContentWarp컴포넌트 추가
     PlayerContentWarp컴포넌트와 css 추가

---
+++ src/components/player/PlayerContent.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./PlayerContent.module.css";
+
+const PlayerContentWarp = () => {
+  return <div className={styles.videoContentWrap}></div>;
+};
+
+export default PlayerContentWarp;
