6ceded57d6138daf51f12dce5ef7bc47c0c810fb
DokyungYou <willow74rr@gmail.com>
2024-06-10 12:35:59 +0900

    Feat(player): Video 컴포넌트 추가
    PlayerContentWarp에 Video 컴포넌트 추가

--- src/components/player/PlayerContent.tsx
+++ src/components/player/PlayerContent.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import styles from "./PlayerContent.module.css";
 import BottomContents from "./bottomContents/BottomContents";
+import Video from "./Video";
 
 const PlayerContentWarp = () => {
   return (
     <div className={styles.videoContentWrap}>
+      <Video />
       <BottomContents />
     </div>
   );
---
+++ src/components/player/Video.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Video = () => {
+  return <div>{/*비디오를 넣어야한다.. */}</div>;
+};
+
+export default Video;
