da0774f6fd8e3e6e3ce93289a15214b930f1dd9a
DokyungYou <willow74rr@gmail.com>
2024-06-11 14:34:09 +0900

    Refactor(palyer): VideoDetail 컴포넌트 수정
    - VideoDetail 컴포넌트의 CSS 클래스 이름을 styles.videoDetailBox로 수정
    - 조건부렌더링의 Notice 컴포넌트에 props 추가

--- src/components/player/videoDetail/VideoDetail.tsx
+++ src/components/player/videoDetail/VideoDetail.tsx
@@ -10,7 +10,7 @@ const VideoDetail:React.FC<VideoDetailProps> = (props) => {
   // 더보기 버튼 클릭 시 상세정보 뜨게해야함
 
   return (
-      <div className={styles.vedioDetailBox}>
+      <div className={styles.videoDetailBox}>
         <div className={styles.logoBox}>
           <img
               src={props.titleImage.titleImageUrl}
@@ -21,10 +21,15 @@ const VideoDetail:React.FC<VideoDetailProps> = (props) => {
                       ageRestriction={props.metadataList.ageRestriction}
                       category={props.metadataList.category}
                       isClosedCaptions={props.metadataList.isClosedCaptions}/>
+        
+        {/*CommentList 필요 (없을 수도 있는 값임)*/}
+
         <EssentialBox />
         <div>{props.summary} <button>더보기</button></div>
 
-        {props.notice && <Notice/>}
+        {props.notice && <Notice title={props.notice.title}
+                                 date={props.notice.date}
+                                 content={props.notice.content}/>}
       </div>
   );
 };
