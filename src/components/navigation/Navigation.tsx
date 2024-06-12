f312d1ff943aade8e6702c4e157079ebb0a3c3ea
many_u <39545238+rha0@users.noreply.github.com>
2024-06-12 15:09:29 +0900

    Design(Category): header 부분 수정
    - utilmenu부분 컴포넌트 분리 (utilmenu 삭제)
    - 검색창 css 추가

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -4,95 +4,66 @@ import styles from "./Navigation.module.css";
 
 
 const Navigation = () => {
-    const [couponShow, setCouponShow] = useState(false);
+
     const [cateShow, setCateShow] = useState(false);
 
 
     return (
-
         <header>
             <div id={styles.headerNav}>
-                <div className={styles.navWrap}>
-                    <ul className={styles.naveWrapList}>
-                        <li><Link to="#">로그인</Link></li>
-                        <li><Link to="#">이용권</Link></li>
-                        <li className={couponShow ? `${styles.over} ${styles.couponMenu}` : styles.couponMenu}
-                            onMouseOver={() => {
-                                setCouponShow(true)
-                            }}
-                            onMouseOut={() => {
-                                setCouponShow(false)
-                            }}
-                        >
-                            <Link to="#">쿠폰·코인</Link>
-                            <div className={styles.couponMenuWrap}
-                                 onMouseOut={() => {
-                                     setCouponShow(false)
-                                 }}
-                            >
-                                <ul>
-                                    <li><Link to="#">쿠폰 등록</Link></li>
-                                    <li><Link to="#">코인 충전</Link></li>
-                                </ul>
-                            </div>
-                        </li>
-                        <li><Link to="#">웨이브온</Link></li>
-                        <li><Link to="#">이벤트</Link></li>
-                    </ul>
-                </div>
-            </div>
-            <div id={styles.headerGnb}>
-                <div className={styles.gnbWrap}>
-                    <h1 className={styles.logo}>
-                        <Link to="#">
-                            <img
-                                src="https://www.wavve.com/img/ci-wavve.5b304973.svg"
-                                alt="웨이브로고"
-                            />
-                        </Link>
-                    </h1>
-                    <ul>
-                        <li><Link to="#">홈</Link></li>
-                        <li className={cateShow ? styles.cateOver : styles.cate}
-                            onMouseOver={() => {
-                                setCateShow(true)
-                            }}
-                            onMouseOut={() => {
-                                setCateShow(false)
-                            }}>
-                            <Link to="#">카테고리</Link>
-                            <div className={styles.categoryMenu}
-                                 onMouseOut={() => {
-                                     setCateShow(false)
-                                 }}
-                            >
-                                <button>전체 카테고리 보기</button>
-                                <ul>
-                                    <li><Link to="#">추천메뉴</Link></li>
-                                    <li><Link to="#">ORIGINAL</Link></li>
-                                    <li><Link to="#">CLASSIC</Link></li>
-                                    <li><Link to="#">드라마</Link></li>
-                                    <li><Link to="#">예능</Link></li>
-                                    <li><Link to="#">시사교양</Link></li>
-                                    <li><Link to="#">애니메이션</Link></li>
-                                    <li><Link to="#">해외시리즈</Link></li>
-                                    <li><Link to="#">영화</Link></li>
-                                    <li><Link to="#">영화플러스</Link></li>
-                                </ul>
-                            </div>
-                        </li>
-                        <li><Link to="#">LIVE</Link></li>
-                        <li><Link to="#">MY</Link></li>
-                    </ul>
-
-                    <div className={styles.searchWrap}>
-                        <label htmlFor="search">
-                            <span>검색</span>
-                        </label>
-                        <input type="text" id="search" placeholder="제목, 장르를 입력하세요"/>
-                        <button>
-                            <span>삭제</span>
-                        </button>
+                <div className={styles.headerGnbWrap}>
+                    <div className={styles.gnbWrap}>
+                        <h1 className={styles.logo}>
+                            <Link to="#">
+                                <img
+                                    src="https://www.wavve.com/img/ci-wavve.5b304973.svg"
+                                    alt="웨이브로고"
+                                />
+                            </Link>
+                        </h1>
+                        <ul>
+                            <li><Link to="#">홈</Link></li>
+                            <li className={cateShow ? styles.cateOver : styles.cate}
+                                onMouseOver={() => {
+                                    setCateShow(true)
+                                }}
+                                onMouseOut={() => {
+                                    setCateShow(false)
+                                }}>
+                                <Link to="#">카테고리</Link>
+                                <div className={styles.categoryMenu}
+                                     onMouseOut={() => {
+                                         setCateShow(false)
+                                     }}
+                                >
+                                    <button>전체 카테고리 보기</button>
+                                    <ul>
+                                        <li><Link to="#">추천메뉴</Link></li>
+                                        <li><Link to="#">ORIGINAL</Link></li>
+                                        <li><Link to="#">CLASSIC</Link></li>
+                                        <li><Link to="#">드라마</Link></li>
+                                        <li><Link to="#">예능</Link></li>
+                                        <li><Link to="#">시사교양</Link></li>
+                                        <li><Link to="#">애니메이션</Link></li>
+                                        <li><Link to="#">해외시리즈</Link></li>
+                                        <li><Link to="#">영화</Link></li>
+                                        <li><Link to="#">영화플러스</Link></li>
+                                    </ul>
+                                </div>
+                            </li>
+                            <li><Link to="#">LIVE</Link></li>
+                            <li><Link to="#">MY</Link></li>
+                        </ul>
+                        <div className={`${styles.searchBox} ${styles.active}`}>
+                            <label htmlFor="search" className={styles.btnSearch}>
+                                <span className={styles.a11yHidden}>검색</span>
+                            </label>
+                            <input type="text" id="search" name="search" placeholder="제목, 장르, 배우로 찾아보세요"
+                                   className={styles.searchInput}/>
+                            <p className={styles.btnDel}>
+                                <span className={styles.a11yHidden}>검색어 삭제</span>
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
