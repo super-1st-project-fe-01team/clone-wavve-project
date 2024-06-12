c183e50256f33aa51e12b6d21f8844e13c917b07
DokyungYou <willow74rr@gmail.com>
2024-06-12 12:39:13 +0900

    Remove(player): Video.tsx 삭제

--- src/components/player/Video.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import {VideoProps} from "../../models/Player";
-
-const Video:React.FC<VideoProps> = (props) => {
-  return <div>{props.videoUrl}</div>;
-};
-
-export default Video;
