70635b113faea2dbf1ff7125d9d78fe741f4ddb1
DokyungYou <willow74rr@gmail.com>
2024-06-13 12:49:43 +0900

    Design(player): Preview.tsx 구조 수정

--- src/components/player/bottomContents/preview/Preview.tsx
+++ src/components/player/bottomContents/preview/Preview.tsx
@@ -4,17 +4,9 @@ import {PreviewProps} from "../../../../models/Player";
 
 const Preview:React.FC<PreviewProps> = (props) => {
   return (
-      <div className={styles.previewViewContent}>
-        <div className={styles.previewDetailBox}>
-          <div className={styles.previewTitle}>커넥션</div>
-          <div className={styles.previewsubTitle}>
-            {props.subtitle}
-          </div>
-          <div className={styles.previewExplanation}>
-            {props.description}
-          </div>
-          <div className={styles.actorInfo}>출연: {props.castings}</div>
-        </div>
+      <div>
+        <div>{props.title}</div>
+        <div>{props.synopsis}</div>
       </div>
   );
 };
