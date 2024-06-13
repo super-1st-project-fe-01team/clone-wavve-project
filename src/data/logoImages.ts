c00631588c301fd5f13a7eb2d47d3826c0c45e59
DokyungYou <willow74rr@gmail.com>
2024-06-13 12:53:14 +0900

    Player에 사용할 로고 이미지 리스트 추가

---
+++ src/data/logoImages.ts
@@ -0,0 +1,30 @@
+import {type} from "os";
+
+type Age ={
+  [key:number]:string;
+}
+
+// 18, 19 이미지 같음
+export const AGE_IMAGES :Age ={
+  0:"https://www.wavve.com/img/element-icons-72-x-72-ic-movie-all-years.97a01b31.svg",
+  12:"https://www.wavve.com/img/element-icons-72-x-72-ic-movie-12-years.3def62d7.svg",
+  15:"https://www.wavve.com/img/element-icons-72-x-72-ic-movie-15-years.d7071acc.svg",
+  18: "https://www.wavve.com/img/element-icons-72-x-72-ic-movie-19-years.33b52775.svg",
+  19:"https://www.wavve.com/img/element-icons-72-x-72-ic-movie-19-years.33b52775.svg",
+};
+
+
+export const DELIVERATION_INFO_IMAGES :string[]=[
+    "https://www.wavve.com/img/element-icons-72-x-72-ic-rating-content-theme.f9b7e623.svg",
+    "https://www.wavve.com/img/element-icons-72-x-72-ic-rating-content-sexuality.65d754cd.svg",
+    "https://www.wavve.com/img/element-icons-72-x-72-ic-rating-content-violence.52b00685.svg",
+    "https://www.wavve.com/img/element-icons-72-x-72-ic-rating-content-dialogue.92f04d3f.svg",
+    "https://www.wavve.com/img/element-icons-72-x-72-ic-rating-content-horror.d3a4e071.svg",
+    "https://www.wavve.com/img/element-icons-72-x-72-ic-rating-content-drug.2af18a6b.svg,",
+    "https://www.wavve.com/img/element-icons-72-x-72-ic-rating-content-imitation.b4fb59a4.svg",
+];
+
+
+export const LIKE_IMAGE:string ="https://www.wavve.com/img/element-icons-30-x-30-ic-big-like-on.c1f4e9c7.svg";
+export const NON_LIKE_IMAGE:string ="https://www.wavve.com/img/element-icons-30-x-30-ic-big-like-off.88996165.svg";
+export const SHARE_IMAGE:string ="https://www.wavve.com/img/element-icons-30-x-30-ic-detail-share.0ce04411.svg";
