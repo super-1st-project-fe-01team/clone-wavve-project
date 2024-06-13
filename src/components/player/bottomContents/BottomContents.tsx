0ace2c75d17446e1e27c380b035804e4e5fa0b08
DokyungYou <willow74rr@gmail.com>
2024-06-13 12:36:33 +0900

    Design(player): BottomContents 구조 , css 수정

--- src/components/player/bottomContents/BottomContents.tsx
+++ src/components/player/bottomContents/BottomContents.tsx
@@ -3,23 +3,44 @@ import PlayerNavigation from "./navigation/PlayerNavigation";
 import Preview from "./preview/Preview";
 import DetailView from "./detailView/DetailView";
 import {BottomContentsProps} from "../../../models/Player";
+import styles from "./bottomView.module.css";
 
 const BottomContents:React.FC<BottomContentsProps> = (props) => {
+
+  // 꼬였다..
+
+  // 임시
+  const image:string="https://image.wavve.com/v1/thumbnails/240_360_20_80/meta/image/202404/1714110749654413518.webp";
+
   return (
       <div>
         <PlayerNavigation />
-        <Preview title={props.preview.title}
-                 subtitle={props.preview.subtitle}
-                 description={props.preview.description}
-                 castings={props.preview.castings}/>
+        <div>
 
-        {/* 시간상 문제로 우선 상세정보 컴포넌트만 고정적으로 나오게 구현계획 */}
-        <DetailView thumbnailImage={props.detailView.thumbnailImage}
-                    summary={props.detailView.summary}
-                    genreTags={props.detailView.genreTags}
-                    castings={props.detailView.castings}
-                    director={props.detailView.director}
-                    ageRestriction={props.detailView.ageRestriction}/>
+          <div className={styles.detailViewBox}>
+            <div className={styles.thumbnailContainer}> <img src={image}/> </div>
+
+            <div className={styles.detailInfoBox}>
+              {/*<Preview title={props.preview.title}*/}
+              {/*         synopsis={props.preview.synopsis}*/}
+              {/*/>*/}
+
+              {/* 시간상 문제로 우선 상세정보 컴포넌트만 고정적으로 나오게 구현계획 */}
+              <DetailView title={props.detailView.title}
+                          synopsis={props.detailView.synopsis}
+                          summary={props.detailView.summary}
+                          genreTags={props.detailView.genreTags}
+                          castings={props.detailView.castings}
+                          directors={props.detailView.directors}
+                          targetAge={props.detailView.targetAge}
+                          deliverationInfo={props.detailView.deliverationInfo}
+              /></div>
+
+          </div>
+        </div>
+
+
+
       </div>
   );
 };
