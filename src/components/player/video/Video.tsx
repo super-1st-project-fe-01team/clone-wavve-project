abb90e619b658cc822fc10ddfb6ea5f27cf4e3cf
DokyungYou <willow74rr@gmail.com>
2024-06-13 12:33:17 +0900

    Remove(player): Notice.tsx, VideoPicture.tsx 삭제

--- src/components/player/video/Video.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import {VideoPictureProps} from "../../../models/Player";
-import styles from "./video.module.css";
-
-const VideoPicture:React.FC<VideoPictureProps> = (props) => {
-  return <div className={styles.videoContainer}><img src={props.videoPictureUrl}/></div>;
-};
-
-export default VideoPicture;
