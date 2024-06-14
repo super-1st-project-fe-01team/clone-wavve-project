4bccb9ce8a9eeab0e22c1027270986c810954fd1
many_u <39545238+rha0@users.noreply.github.com>
2024-06-14 10:42:40 +0900

    Design(HeaderFilter): width값 변경
    - width값 변경 및 스타일 수정

--- src/components/Live/HeaderFilter.tsx
+++ src/components/Live/HeaderFilter.tsx
@@ -2,41 +2,46 @@ import React, {useState} from "react";
 import styles from './HeaderFilter.module.css'
 
 const categories = [
-  '전체 장르',
-  '정주행 채널 (무료)',
-  '지상파',
-  '종편/보도',
-  '홈쇼핑 (무료)',
-  '드라마/예능',
-  '시사/교양',
-  '영화/스포츠',
-  '키즈/애니',
-  '라디오/음악'
+    '전체 장르',
+    '정주행 채널 (무료)',
+    '지상파',
+    '종편/보도',
+    '홈쇼핑 (무료)',
+    '드라마/예능',
+    '시사/교양',
+    '영화/스포츠',
+    '키즈/애니',
+    '라디오/음악'
 ];
 
 interface FilterHeaderProps {
-  onCategoryChange: (category: string) => void;
+    onCategoryChange: (category: string) => void;
 }
 
 const HeaderFilter: React.FC<FilterHeaderProps> = ({onCategoryChange}) => {
-  const [selectCategory, setSelectCategory] = useState <string>('전체 장르');
+    const [selectCategory, setSelectCategory] = useState<string>('전체 장르');
 
-  const filterChangeHandler = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    const category = event.target.value;
-    setSelectCategory(category);
-    onCategoryChange(category);
-  };
+    const filterChangeHandler = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        const category = event.target.value;
+        setSelectCategory(category);
+        onCategoryChange(category);
+    };
 
-  return <div className={styles.filterBox}>
-    <div className={styles.filterText}>전체 채널</div>
-    <select className={styles.filter} value={selectCategory} onChange={filterChangeHandler}>
-      {categories.map((category, index) => (
-          <option key={index} value={category}>
-            {category}
-          </option>
-      ))}
-    </select>
-  </div>;
+    return (
+        <div className={styles.filterBoxWrap}>
+            <div className={styles.filterBox}>
+                <div className={styles.filterText}>전체 채널</div>
+                <select className={styles.filter} value={selectCategory} onChange={filterChangeHandler}>
+                    {categories.map((category, index) => (
+                        <option key={index} value={category}>
+                            {category}
+                        </option>
+                    ))}
+                </select>
+            </div>
+        </div>
+    );
 };
+
 
 export default HeaderFilter;
