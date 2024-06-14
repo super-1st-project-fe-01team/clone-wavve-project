fe70dab4124d10f9e58917a5deb3f79fdc7d2dbd
many_u <39545238+rha0@users.noreply.github.com>
2024-06-14 10:43:13 +0900

    Design(HeaderIcon): width값 변경
    - width값 변경 및 css 스타일 수정

--- src/components/Live/HeaderIcon.tsx
+++ src/components/Live/HeaderIcon.tsx
@@ -2,26 +2,28 @@ import React from "react";
 import styles from "./HeaderIcon.module.css";
 
 const HeaderIcon: React.FC = () => {
-  return (
-      <div className={styles.iconBar}>
-        <div className={styles.text}>LIVE</div>
-        <div className={styles.menu}>
-          <img
-              className={styles.icon}
-              src="
+    return (
+        <div className={styles.iconBarWrap}>
+            <div className={styles.iconBar}>
+                <div className={styles.text}>LIVE</div>
+                <div className={styles.menu}>
+                    <img
+                        className={styles.icon}
+                        src="
           https://www.wavve.com/img/icon-alart-off.d0007c58.svg"
-              alt={''}
-          />
-          <div>시청예약</div>
-          <img
-              className={styles.icon}
-              src="https://www.wavve.com/img/ic-detail-schd-off.4b017899.svg"
-              alt={''}
-          />
-          <div>편성표</div>
+                        alt={''}
+                    />
+                    <div>시청예약</div>
+                    <img
+                        className={styles.icon}
+                        src="https://www.wavve.com/img/ic-detail-schd-off.4b017899.svg"
+                        alt={''}
+                    />
+                    <div>편성표</div>
+                </div>
+            </div>
         </div>
-      </div>
-  );
+    );
 };
 
 export default HeaderIcon;
