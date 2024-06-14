7bdaf9be1a44be9ab66bafd5f7b70b7b69045b4b
DokyungYou <willow74rr@gmail.com>
2024-06-14 22:03:34 +0900

    Comment(player): 추후 적용해야 할 부분 주석추가

--- src/components/player/bottomContents/navigation/PlayerNavigation.tsx
+++ src/components/player/bottomContents/navigation/PlayerNavigation.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import styles from "./PlayerNavigation.module.css";
 
 const PlayerNavigation = () => {
+
+  //TODO:
+  // 다시 확인해보니 작품에 따라 버튼 구성이 다름 (추천, 상세정보는 기본)
+  // player 인터페이스에  관련영상, 시리즈를 가지고 있는지에 대한 속성 정보가 필요
   return (
       <div>
         <nav className={styles.playerNav}>
