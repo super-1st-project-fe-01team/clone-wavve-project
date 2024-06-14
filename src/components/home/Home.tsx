cfa57aa5f74c49913a6ff048ec1097356e00c3be
many_u <39545238+rha0@users.noreply.github.com>
2024-06-14 09:11:05 +0900

    Design(Home): 카테고리 리스트 영역 추가
    - 카테고리 리스트 영역 추가

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -4,7 +4,7 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import Footer from "../footer/Footer"
-
+import MainCategory from "../category/MainCategory";
 
 
 const Home = () => {
@@ -46,6 +46,7 @@ const Home = () => {
                     </Slider>
                 </div>
             </section>
+            <MainCategory/>
             <Footer/>
         </div>
     );
